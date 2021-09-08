12a6a1959bba0ab0f3ff7238c0567925dfb51134
Neiro Somemiya <nirot1r@g-second.net>
2021-09-08 22:03:33 +0900

    API仮実装

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,27 +4,34 @@ dotenv.config();
 import Express from 'express';
 import cors from 'cors';
 
-import Core from './libs/core';
+import Mongo from 'mongodb';
+import {
+  Db,
+  Container,
+  Product,
+  Device,
+  genre as GenreArray,
+} from './libs/core';
 
 class Souko {
   private _app: Express.Application;
-  private _core: Core;
+  private _db: Db;
 
   constructor(o: { dbUri: string | undefined; dbName: string | undefined }) {
     if (!o.dbUri) throw new Error('データベースURIが入力されていません。');
     if (!o.dbName) throw new Error('データベース名が入力されていません。');
 
     this._app = Express();
-    this._core = new Core(o.dbUri, o.dbName);
+    this._db = new Db(o.dbUri, o.dbName);
   }
 
   public start(): void {
     this._corsMiddleware();
     this._bodyParserMiddleware();
 
-    this._indexRoute();
+    this._routes();
 
-    this._core.start();
+    this._db.start();
     this._app.listen(53894, () => console.log(`Port 53894 listening...`));
   }
 
@@ -37,10 +44,161 @@ class Souko {
     this._app.use(Express.json());
   }
 
+  private _routes(): void {
+    this._indexRoute();
+
+    this._addContainerRoute();
+    this._addProductRoute();
+    this._addDeviceRoute();
+
+    this._getContainerWithCodeRoute();
+    this._getDeviceWithCodeRoute();
+
+    this._searchProductsRoute();
+  }
+
   private _indexRoute(): void {
     this._app.get('/', (req: Express.Request, res: Express.Response) => {
       return res.json({ msg: 'N-Souko' });
     });
+  }
+
+  private _addContainerRoute(): void {
+    this._app.post(
+      '/containers',
+      (req: Express.Request, res: Express.Response) => {
+        this._db
+          .addContainer({
+            storageId: req.body.storageId,
+            name: req.body.name,
+          })
+          .then((o: { containerId: Mongo.ObjectId; code: string }) => {
+            return res.json(o);
+          })
+          .catch((err: Error) => {
+            return res.status(503).json({ error: err.message });
+          });
+      }
+    );
+  }
+
+  private _addProductRoute(): void {
+    this._app.post(
+      '/products',
+      (req: Express.Request, res: Express.Response) => {
+        this._db
+          .addProduct({
+            name: req.body.name,
+            maker: { name: req.body.name },
+            genre: req.body.genre,
+          })
+          .then((_id: Mongo.ObjectId) => {
+            return res.json({ productId: _id });
+          })
+          .catch((err: Error) => {
+            return res.status(503).json({ error: err.message });
+          });
+      }
+    );
+  }
+
+  private _addDeviceRoute(): void {
+    this._app.post(
+      '/devices',
+      (req: Express.Request, res: Express.Response) => {
+        this._db
+          .addDevice({
+            productId: req.body.productId,
+            containerId: req.body.containerId,
+            status: req.body.status,
+            serialNumber: req.body.serialNumber,
+            remarks: req.body.remarks,
+          })
+          .then((o: { deviceId: Mongo.ObjectId; code: string }) => {
+            return res.json(o);
+          })
+          .catch((err: Error) => {
+            return res.status(503).json({ error: err.message });
+          });
+      }
+    );
+  }
+
+  private _getContainerWithCodeRoute(): void {
+    this._app.get(
+      '/containers/code/:containerCode',
+      (req: Express.Request, res: Express.Response) => {
+        this._db
+          .getContainerWithCode(req.params.containerCode)
+          .then(
+            async (o: { container: Container | null; devices: Device[] }) => {
+              const result = {
+                container: o.container,
+                devices: await Promise.all(
+                  o.devices.map(async (v: Device) => {
+                    return {
+                      _id: v._id,
+                      code: v.code,
+                      status: v.status,
+                      serialNumber: v.serialNumber,
+                      remarks: v.remarks,
+                      product: await this._db.getProductWithId(v.productId),
+                    };
+                  })
+                ),
+              };
+              return res.json(result);
+            }
+          )
+          .catch((err) => {
+            throw err;
+          });
+      }
+    );
+  }
+
+  private _getDeviceWithCodeRoute(): void {
+    this._app.get(
+      '/devices/code/:deviceCode',
+      (req: Express.Request, res: Express.Response) => {
+        this._db
+          .getDeviceWithCode(req.params.deviceCode)
+          .then((o: Device | null) => {
+            return res.json(o);
+          })
+          .catch((err) => {
+            throw err;
+          });
+      }
+    );
+  }
+
+  private _searchProductsRoute(): void {
+    this._app.get(
+      '/products/search',
+      (req: Express.Request, res: Express.Response) => {
+        const genre =
+          typeof req.query.genre === 'string' &&
+          GenreArray.includes(req.query.genre)
+            ? { genre: req.query.genre }
+            : {};
+
+        this._db
+          .findProductsWithQuery({
+            $or: [
+              { name: req.query.query },
+              { maker: { name: req.query.query } },
+            ],
+            $and: [genre],
+          })
+          .then((o: Product[]) => {
+            return res.json(o);
+          })
+          .catch((err) => {
+            throw err;
+          });
+      }
+    );
   }
 }
 
--- server/src/libs/core.ts
+++ server/src/libs/core.ts
@@ -1,6 +1,54 @@
-import Mongo, { MongoClient } from 'mongodb';
+import { countReset } from 'console';
+import * as Mongo from 'mongodb';
+import { PrefixUnaryOperator } from 'typescript';
+// const Mongo = require('mongodb');
 
-export default class Core {
+export const genre = [
+  'broadcast',
+  'pa',
+  'venue',
+  'pr',
+  'transpotation',
+  'oa',
+  'other',
+];
+export type Genre = typeof genre[number];
+
+export const deviceStatus = ['available', 'failure', 'reparing', 'remove'];
+export type DeviceStatus = typeof deviceStatus[number];
+
+export type Storage = {
+  _id: Mongo.ObjectId;
+  name: string;
+};
+
+export type Container = {
+  _id: Mongo.ObjectId;
+  storageId: string;
+  code?: string;
+  name: string;
+};
+
+export type Product = {
+  _id: Mongo.ObjectId;
+  genre: Genre;
+  name: string;
+  maker: {
+    name: string;
+  };
+};
+
+export type Device = {
+  _id: Mongo.ObjectId;
+  productId: string;
+  containerId: string;
+  code?: string;
+  status: DeviceStatus;
+  serialNumber: string;
+  remarks: string;
+};
+
+export class Db {
   private _db!: Mongo.Db;
 
   private _dbUri: string;
@@ -11,14 +59,186 @@ export default class Core {
     this._dbName = dbName;
   }
 
-  public start(): void {
-    Mongo.MongoClient.connect(this._dbUri)
-      .then((mongo: MongoClient) => {
-        this._db = mongo.db(this._dbName);
-        return;
+  public async start(): Promise<void> {
+    this._db = (
+      await Mongo.MongoClient.connect(this._dbUri).catch((err) => {
+        throw err;
       })
+    ).db(this._dbName);
+  }
+
+  public async addStorage(o: { name: string }): Promise<Mongo.ObjectId> {
+    const result = await this._db
+      .collection<Storage>('storages')
+      .insertOne(o)
       .catch((err) => {
         throw err;
       });
+
+    return result.insertedId;
+  }
+
+  public async addContainer(o: {
+    storageId: string;
+    name: string;
+  }): Promise<{ containerId: Mongo.ObjectId; code: string }> {
+    const containerId = await this._createContainer(o);
+    const count = await this._countContainers();
+    const code = this._generate4DigitCode(count);
+
+    this._applyContainerCode(containerId, code);
+    return { containerId, code };
+  }
+
+  private async _createContainer(o: {
+    storageId: string;
+    name: string;
+  }): Promise<Mongo.ObjectId> {
+    const result = await this._db
+      .collection<Container>('containers')
+      .insertOne(o)
+      .catch((err) => {
+        throw err;
+      });
+
+    return result.insertedId;
+  }
+
+  private async _countContainers(): Promise<number> {
+    const containers = await this._db
+      .collection<Container>('containers')
+      .find({})
+      .toArray();
+
+    return containers.length;
+  }
+
+  private async _applyContainerCode(
+    containerId: Mongo.ObjectId,
+    code: string
+  ): Promise<void> {
+    this._db
+      .collection<Container>('containers')
+      .updateOne({ _id: containerId }, { $set: { code } });
+  }
+
+  public async addProduct(o: {
+    name: string;
+    maker: { name: string };
+    genre: Genre;
+  }): Promise<Mongo.ObjectId> {
+    const result = await this._db
+      .collection<Product>('products')
+      .insertOne(o)
+      .catch((err) => {
+        throw err;
+      });
+
+    return result.insertedId;
+  }
+
+  public async addDevice(o: {
+    productId: string;
+    containerId: string;
+    status: DeviceStatus;
+    serialNumber: string;
+    remarks: string;
+  }): Promise<{ deviceId: Mongo.ObjectId; code: string }> {
+    const deviceId = await this._createDevice(o);
+    const count = await this._countDevices();
+    const code = this._generate4DigitCode(count);
+
+    this._applyDeviceCode(deviceId, code);
+    return { deviceId, code };
+  }
+
+  private async _createDevice(o: {
+    productId: string;
+    containerId: string;
+    status: DeviceStatus;
+    serialNumber: string;
+    remarks: string;
+  }): Promise<Mongo.ObjectId> {
+    const result = await this._db
+      .collection<Device>('devices')
+      .insertOne(o)
+      .catch((err) => {
+        throw err;
+      });
+
+    return result.insertedId;
+  }
+
+  private async _countDevices(): Promise<number> {
+    const containers = await this._db
+      .collection<Device>('devices')
+      .find({})
+      .toArray();
+
+    return containers.length;
+  }
+
+  private async _applyDeviceCode(
+    deviceId: Mongo.ObjectId,
+    code: string
+  ): Promise<void> {
+    this._db
+      .collection<Device>('devices')
+      .updateOne({ _id: deviceId }, { $set: { code } });
+  }
+
+  private _generate4DigitCode(id: number): string {
+    return ('000000' + id).slice(-6);
+  }
+
+  public async getContainerWithCode(containerCode: string): Promise<{
+    container: Container | null;
+    devices: Device[];
+  }> {
+    const container = await this._db
+      .collection<Container>('containers')
+      .findOne({ code: containerCode }, { sort: { _id: -1 } });
+
+    if (!container) {
+      throw new Error(`${containerCode} container not found`);
+    }
+    console.log({ containerId: container._id.toHexString() });
+
+    const devices = await this._db
+      .collection<Device>('devices')
+      .find({ containerId: container._id.toHexString() })
+      .toArray();
+
+    return {
+      container,
+      devices,
+    };
+  }
+
+  public async getProductWithId(productId: string): Promise<Product | null> {
+    const product = await this._db
+      .collection<Product>('products')
+      .findOne({ _id: new Mongo.ObjectId(productId) });
+
+    return product;
+  }
+
+  public async getDeviceWithCode(deviceCode: string): Promise<Device | null> {
+    const device = await this._db
+      .collection<Device>('devices')
+      .findOne({ code: deviceCode });
+
+    return device;
+  }
+
+  public async findProductsWithQuery(
+    query: Mongo.Filter<Product>
+  ): Promise<Product[]> {
+    const products = await this._db
+      .collection<Product>('products')
+      .find(query)
+      .toArray();
+
+    return products;
   }
 }
